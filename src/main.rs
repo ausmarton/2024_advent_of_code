22f2dd0ad04b0069bbf2900408a8fb982a9cd11a
Ausmarton Fernandes <ausmarton@fernandes.io>
2024-12-02 21:54:42 +0000

    Day 2 - part 2: Brute force, needs to be refactored

--- src/main.rs
+++ src/main.rs
@@ -23,6 +23,11 @@ fn main() {
         Ok(count) => println!("Safe reports: {}", count),
         Err(e) => eprintln!("Error encountered in file {}", e)
     }
+
+    match count_dampened_safe_reports(day_2_input_file) {
+        Ok(count) => println!("Safe Dampened reports: {}", count),
+        Err(e) => eprintln!("Error encountered in file {}", e)
+    }
 }
 
 fn total_distance(file_path: &str) -> io::Result<(i32, i32)> {
@@ -80,11 +85,62 @@ fn count_safe_reports(file_path: &str) -> io::Result<usize> {
     }
 }
 
+//day 2 - part 2
+fn count_dampened_safe_reports(file_path: &str) -> io::Result<usize> {
+    match read_lines(file_path) {
+        Ok(lines) => Ok(lines.filter_map(Result::ok)
+                        .map(|line| line.split_whitespace()
+                        .filter_map(|s| s.parse::<i32>().ok()).collect())
+                        .filter(|report| is_safe_report_dampened(report)).count()),
+        Err(e) => Err(e)
+
+    }
+}
+
+
 fn is_safe_report(report: &Vec<i32>) -> bool {
+    let ordered = if is_ascending(report) {
+        report
+    } else {
+        &report.iter().rev().cloned().collect()
+    };
+    is_increasing_gradually(&ordered)
+}
+
+fn is_increasing_gradually(report: &Vec<i32>) -> bool {
     report.windows(2)
-        .all(|w| w[0] < w[1] && (w[0]+4) > w[1]) ||
-        report.windows(2)
-            .all(|w| w[0] > w[1] && (w[0]-4) < w[1])
+    .all(|w| w[0] < w[1] && (w[0]+4) > w[1])
+}
+
+fn is_ascending(report: &Vec<i32>) -> bool {
+    report.windows(2)
+    .filter(|w| w[0] < w[1])
+    .count() > report.windows(2)
+    .filter(|w| w[0] > w[1])
+    .count() 
+}
+
+fn is_safe_report_dampened(report: &Vec<i32>) -> bool {
+    let ordered = if is_ascending(report) {
+        report
+    } else {
+        &report.iter().rev().cloned().collect()
+    };
+
+    if is_increasing_gradually(&ordered) {
+        return true;
+    }
+
+    (0..ordered.len())
+    .map(|i| {
+        ordered
+            .iter()
+            .enumerate()
+            .filter(|&(index, _)| index != i)
+            .map(|(_, &value)| value)
+            .collect::<Vec<i32>>()
+    })
+    .any(|v| is_increasing_gradually(&v))
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
